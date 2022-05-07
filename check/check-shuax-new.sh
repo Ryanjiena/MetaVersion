4e1d3f8e2f883b5808af94b231815f28926b030b
Ryanjiena <43134606+Ryanjiena@users.noreply.github.com>
2022-05-07 19:22:18 +0800

    chore: fix shuax scripts

--- check/check-shuax-new.sh
+++ check/check-shuax-new.sh
@@ -15,8 +15,8 @@ Tip="${Green_font_prefix}[Tip]${Font_color_suffix}"
 chrome_plus_remote_ver="$(wget --user-agent="${userAgent}" --no-check-certificate -qO- "https://api.github.com/repos/shuax/chrome_plus/actions/workflows/build.yml/runs?branch=main&status=success" | grep -o '"created_at": ".*"' | head -n 1 | sed 's/"//g;s/-//g;s/://g' | sed 's/created_at //g;s/T/./g;s/Z//g')"
 edge_plus_remote_ver="$(wget --user-agent="${userAgent}" --no-check-certificate -qO- "https://api.github.com/repos/shuax/edge_plus/actions/workflows/build.yml/runs?branch=main&status=success" | grep -o '"created_at": ".*"' | head -n 1 | sed 's/"//g;s/-//g;s/://g' | sed 's/created_at //g;s/T/./g;s/Z//g')"
 
-chrome_plus_local_ver="$(cat ../shuax | grep -Po "chrome_plus-x64-v[\d.]+\.zip" | sed 's/chrome_plus-x64-v//g;s/\.zip//g' | head -1)"
-edge_plus_local_ver="$(cat ../shuax | grep -Po "edge_plus-x64-v[\d.]+\.zip" | sed 's/edge_plus-x64-v//g;s/\.zip//g' | head -1)"
+chrome_plus_local_ver="$(cat ../shuax | grep -Po "chrome_plus-x64-v[\d.]+\.zip" | sed 's/chrome_plus-x64-v//g;s/\.zip//g' | tail -1)"
+edge_plus_local_ver="$(cat ../shuax | grep -Po "edge_plus-x64-v[\d.]+\.zip" | sed 's/edge_plus-x64-v//g;s/\.zip//g' | tail -1)"
 
 echo "Latest version:  ${chrome_plus_remote_ver}  ${edge_plus_remote_ver}"
 echo "Local version:  ${chrome_plus_local_ver}  ${edge_plus_local_ver}"
@@ -38,14 +38,14 @@ else
     # upload to onedrive
     # wget --user-agent="${userAgent}" --no-check-certificate --quiet -O "sha256sum" "https://pan.jiemi.workers.dev/?file=/scoop/shuax/sha256sum"
     cp ../../shuax sha256sum
-    sha256sum chrome_plus* edge_plus* >> sha256sum
-    awk ' { t = $1; $1 = $2; $2 = t; print; } ' sha256sum | sort -f | awk ' { t = $1; $1 = $2; $2 = t; print; } ' > sha256sum1
+    sha256sum chrome_plus* edge_plus* >>sha256sum
+    awk ' { t = $1; $1 = $2; $2 = t; print; } ' sha256sum | sort -f | awk ' { t = $1; $1 = $2; $2 = t; print; } ' >sha256sum1
     mv sha256sum1 sha256sum -f
     cd ..
     wget --user-agent="${userAgent}" --no-check-certificate --quiet "https://github.com/gaowanliang/LightUploader/releases/download/v2.0.2-fix/LightUploader_Linux_x86_64.tar.gz"
     tar -xzf LightUploader_Linux_x86_64.tar.gz && sudo chmod +x ./LightUploader
     echo ${CONFIG} >config.json
-    ./LightUploader -c config.json -f shuax -r "public/scoop/" -t 6 -b 20 > ${tmpFile}
+    ./LightUploader -c config.json -f shuax -r "public/scoop/" -t 6 -b 20 >${tmpFile}
 
     mv -f shuax/sha256sum ../shuax
     rm -rf shuax/
