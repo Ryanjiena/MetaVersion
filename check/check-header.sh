e18a2a3c8eae5d7fe50d12a79dcd52fcd94b6242
Ryanjiena <43134606+Ryanjiena@users.noreply.github.com>
2022-05-09 08:19:37 +0800

    chore: update header scripts

--- check/check-header.sh
+++ check/check-header.sh
@@ -19,18 +19,23 @@ redirectArr=($(cat ../header.json | jq -r '.[] | .redirect'))
 
 for ((i = 0; i < ${#nameArr[@]}; i++)); do
 
-    curl -v -k --tls-max 1.2 "${urlArr[i]}" 2>${tmpFile} >/dev/null
+    curl -vsS -k --tls-max 1.2 --connect-timeout 3 "${urlArr[i]}" 2>${tmpFile} >/dev/null
     # cat ${tmpFile} && exit 0
 
     redirect_url=$(cat ${tmpFile} | grep -Poi 'location: .*?\.exe' | sed 's/location: //gi')
     # echo ${result} && exit 0
 
     if [[ -n "${redirect_url}" ]]; then
-        echo -e "${Green_font_prefix}${nameArr[i]}${Font_color_suffix} -> ${redirect_url}"
+        echo -e "${Green_font_prefix}${nameArr[i]}-url${Font_color_suffix} -> ${redirect_url}"
         sed -e "s|${redirectArr[i]}|${redirect_url}|g" -i ../header.json
     else
-        echo -e "${Error} ${nameArr[i]} is not exist!" && exit 1
+        echo -e "${Error} ${nameArr[i]} is not exist!" && continue
     fi
 
     rm -f ${tmpFile}
 done
+
+youdaoDictArr=($(cat ../header.json | jq " .[] | select (.name == \"youdao-dict\")" | jq -r ".redirect, .version_redirect, .versionurl"))
+youdaoDictVersion=$(curl -sSL "${youdaoDictArr[2]}" | grep -Po "更新至 .*?:" | head -1 | sed 's/更新至 //g;s/://g')
+echo -e "${Green_font_prefix}youdao-dict-version${Font_color_suffix} -> ${youdaoDictVersion}"
+sed -e "s|${youdaoDictArr[1]}|${youdaoDictVersion}#${youdaoDictArr[0]}|g" -i ../header.json
